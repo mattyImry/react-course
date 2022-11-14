2c3b33d4afe3bff572d5a51a66768d2476f026bb
Matteo Imeri <matteo.imeri@gmail.com>
2022-11-14 17:36:54 +0000

    push test 3

--- react-udemy/src/App.js
+++ react-udemy/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn tnt
+          Learn react now
         </a>
       </header>
     </div>
