8a4a5c725b08beed417aa20348c2533eee198bd7
Matteo Imeri <matteo.imeri@gmail.com>
2022-11-23 12:24:56 +0000

    Add ExpenseForm and ExpenseItem

--- react-udemy/src/components/Expenses/ExpenseItem.js
+++ react-udemy/src/components/Expenses/ExpenseItem.js
@@ -5,6 +5,7 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
     setTitle("Updated");
     console.log(title);
