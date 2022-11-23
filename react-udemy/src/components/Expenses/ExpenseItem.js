d69f11d0bf59a0d829d5209d40c716d591344c1e
Matteo Imeri <matteo.imeri@gmail.com>
2022-11-23 11:42:31 +0000

    set state "title" ExpenseItem

--- react-udemy/src/components/Expenses/ExpenseItem.js
+++ react-udemy/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,20 @@
-import "./ExpenseItem.css";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate.js";
 import Card from "../UI/Card.js";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
-    console.log("clicked!!!");
+    setTitle("Updated");
+    console.log(title);
   };
 
   return (
     <Card className={"expense-item"}>
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>change title</button>
