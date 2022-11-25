967d73a598c73baa70aeccdef667c15f02f6e083
Matteo Imeri <matteo.imeri@gmail.com>
2022-11-25 15:41:31 +0000

    submit form

--- react-udemy/src/components/NewExpense/ExpenseForm.js
+++ react-udemy/src/components/NewExpense/ExpenseForm.js
@@ -19,9 +19,20 @@ const ExpenseForm = () => {
     setEnteredDate(event.target.value);
   }
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+
+    console.log(expenseData);
+  };
 
   return (
-    <form action=""> 
+    <form onSubmit={submitHandler} > 
       <div className="new-expense__controls">
           <div className="new-expense__control">
               <label>Title</label>
@@ -29,10 +40,10 @@ const ExpenseForm = () => {
           </div>
           <div className="new-expense__control">
               <label>Amount</label>
-              <input type="number" min="0.01" max="0.01" onChange={amountChangeHandler} />
+              <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
           </div>
           <div className="new-expense__control">
-              <label>Title</label>
+              <label>Date</label>
               <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
           </div>
           
