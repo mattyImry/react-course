0c8a864324776f8dcf32b1dd6ea6c2c4725c7c26
Matteo Imeri <matteo.imeri@gmail.com>
2022-11-29 18:39:42 +0000

    Add ExpensesList component

--- react-udemy/src/components/Expenses/Expenses.js
+++ react-udemy/src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "../NewExpense/ExpensesFilter";
 
 const Expenses = (props) => {
@@ -15,24 +15,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  //display message if no eexpenses for selected year
-  // may way to do it with ternary operator or &&
-
-  let expensesContent = <p>No Expenses Found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        //when displaying multiple items use a unique identifier
-        //with key={unique identifier}
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -41,38 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
         />
 
-        {expensesContent}
-        {/* USING TERNARY */}
-
-        {/* {filteredExpenses.length === 0 ? (
-          <p>No Exp</p>
-        ) : (
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              //when displaying multiple items use a unique identifier
-              //with key={unique identifier}
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))
-        )} */}
-
-        {/* USING && */}
-
-        {/* {filteredExpenses.length === 0 && <p>No Exp</p>}
-        {filteredExpenses.length > 0 &&
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              //when displaying multiple items use a unique identifier
-              //with key={unique identifier}
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))} */}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
