7b6f06fb005bd02e5e493211ae21cbcb9a8f97de
Matteo Imeri <matteo.imeri@gmail.com>
2022-11-30 15:37:08 +0000

    add ExpensesChart

--- react-udemy/src/components/Chart/Chart.js
+++ react-udemy/src/components/Chart/Chart.js
@@ -2,14 +2,16 @@ import "./Chart.css";
 import ChartBar from "./ChartBar";
 
 const Chart = (props) => {
+    const dataPointValue = props.dataPoints.map((dataPoint) => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointValue);
     return (
         <div className="chart">
             {props.dataPoints.map((dataPoint) => (
                 <ChartBar
                     key={dataPoint.label}
                     value={dataPoint.value}
-                    maxValue={null}
-                    label={dataPoint.lable}
+                    maxValue={totalMaximum}
+                    label={dataPoint.label}
                 />
             ))}
         </div>
--- react-udemy/src/components/Expenses/Expenses.js
+++ react-udemy/src/components/Expenses/Expenses.js
@@ -3,30 +3,31 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "../NewExpense/ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
-  const filteredChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
-  };
+    const [filteredYear, setFilteredYear] = useState("2020");
+    const filteredChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
 
-  //display expenses when year filter is used
-  const filteredExpenses = props.items.filter((expense) => {
-    return expense.date.getFullYear().toString() === filteredYear;
-  });
+    //display expenses when year filter is used
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
 
-  return (
-    <div>
-      <Card className="expenses">
-        <ExpensesFilter
-          onChangeFilter={filteredChangeHandler}
-          selected={filteredYear}
-        />
-
-        <ExpensesList items={filteredExpenses} />
-      </Card>
-    </div>
-  );
+    return (
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter
+                    onChangeFilter={filteredChangeHandler}
+                    selected={filteredYear}
+                />
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses} />
+            </Card>
+        </div>
+    );
 };
 
 export default Expenses;
