e1df3b32f5eea4d22e6577838f407523f476e92c
Matteo Imeri <matteo.imeri@gmail.com>
2022-11-30 11:03:40 +0000

    hide ExpenseForm

--- react-udemy/src/components/NewExpense/ExpenseForm.js
+++ react-udemy/src/components/NewExpense/ExpenseForm.js
@@ -5,6 +5,7 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  const [formDisplayed, setFormDisplayed] = useState(false);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -34,42 +35,62 @@ const ExpenseForm = (props) => {
     setEnteredDate("");
   };
 
+  const displayFormHandler = () => {
+    setFormDisplayed(true);
+  };
+
+  const closeFormHandler = () => {
+    setFormDisplayed(false);
+  };
+
+  if (formDisplayed == true) {
+    return (
+      <React.Fragment>
+        {formDisplayed && (
+          <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+              <div className="new-expense__control">
+                <label>Title</label>
+                <input
+                  type="text"
+                  value={enteredTitle}
+                  onChange={titleChangeHandler}
+                />
+              </div>
+              <div className="new-expense__control">
+                <label>Amount</label>
+                <input
+                  type="number"
+                  min="0.01"
+                  step="0.01"
+                  value={enteredAmount}
+                  onChange={amountChangeHandler}
+                />
+              </div>
+              <div className="new-expense__control">
+                <label>Date</label>
+                <input
+                  type="date"
+                  min="2019-01-01"
+                  max="2022-12-31"
+                  value={enteredDate}
+                  onChange={dateChangeHandler}
+                />
+              </div>
+            </div>
+            <div className="new-expense__actions">
+              <button onClick={closeFormHandler}>Cancel</button>
+              <button type="submit">Add Expense</button>
+            </div>
+          </form>
+        )}
+      </React.Fragment>
+    );
+  }
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+    <div>
+      <button onClick={displayFormHandler}>Add Expense</button>
+    </div>
   );
 };
 
