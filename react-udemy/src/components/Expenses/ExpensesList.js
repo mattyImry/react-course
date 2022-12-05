a21e17253ad24b68d829a92cb91a7002424c6e3a
Matteo Imeri <matteo.imeri@gmail.com>
2022-12-05 12:49:32 +0000

    debugging , check erors

--- react-udemy/src/components/Expenses/ExpensesList.js
+++ react-udemy/src/components/Expenses/ExpensesList.js
@@ -2,36 +2,36 @@ import "./ExpensesList.css";
 import ExpenseItem from "./ExpenseItem";
 
 const ExpensesList = (props) => {
-  //display message if no expenses for selected year
-  // many way to do it with ternary operator or &&
+    //display message if no expenses for selected year
+    // many way to do it with ternary operator or &&
 
-  if (props.items.length === 0) {
-    return <h2 className="expenses-list__fallback">No Expenses Found</h2>;
-  }
+    if (props.items.length === 0) {
+        return <h2 className="expenses-list__fallback">No Expenses Found</h2>;
+    }
 
-  return (
-    <ul className="expenses-list">
-      {props.items.map((expense) => (
-        <ExpenseItem
-          //when displaying multiple items use a unique identifier
-          //with key={unique identifier}
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-      ;
-    </ul>
-  );
+    return (
+        <ul className="expenses-list">
+            {props.items.map((expense) => (
+                <ExpenseItem
+                    //when displaying multiple items use a unique identifier
+                    //with key={unique identifier}
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+            ))}
+            ;
+        </ul>
+    );
 };
 
 {
-  /* USING TERNARY */
+    /* USING TERNARY */
 }
 
 {
-  /* {filteredExpenses.length === 0 ? (
+    /* {filteredExpenses.length === 0 ? (
           <p>No Exp</p>
         ) : (
           filteredExpenses.map((expense) => (
@@ -48,11 +48,11 @@ const ExpensesList = (props) => {
 }
 
 {
-  /* USING && */
+    /* USING && */
 }
 
 {
-  /* {filteredExpenses.length === 0 && <p>No Exp</p>}
+    /* {filteredExpenses.length === 0 && <p>No Exp</p>}
         {filteredExpenses.length > 0 &&
           filteredExpenses.map((expense) => (
             <ExpenseItem
