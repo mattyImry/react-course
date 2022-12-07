4ff0961973bc82539f771bb4bd99f6cd5ff444f2
Matteo Imeri <matteo.imeri@gmail.com>
2022-12-07 13:08:24 +0000

    add ErrorModal.js

--- user-prj/src/components/Users/AddUser.js
+++ user-prj/src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
@@ -35,25 +36,28 @@ const AddUser = (props) => {
     };
 
     return (
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input
-                    id="username"
-                    type="text"
-                    onChange={usernameChangeHandler}
-                    value={enteredUserName}
-                />
-                <label htmlFor="age">Age (Years)</label>
-                <input
-                    id="age"
-                    type="number"
-                    onChange={ageChangeHandler}
-                    value={enteredAge}
-                />
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
+        <div>
+            <ErrorModal title="Error" message="Something wrong" />
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        id="username"
+                        type="text"
+                        onChange={usernameChangeHandler}
+                        value={enteredUserName}
+                    />
+                    <label htmlFor="age">Age (Years)</label>
+                    <input
+                        id="age"
+                        type="number"
+                        onChange={ageChangeHandler}
+                        value={enteredAge}
+                    />
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </div>
     );
 };
 
