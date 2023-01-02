30d7793d1580123dd4e365755114e03bc29c553d
mattyImry <matteo.imeri@gmail.com>
2023-01-02 15:13:00 +0000

    Fixed missing class Cart.js

--- food-app/src/Components/Cart/Cart.js
+++ food-app/src/Components/Cart/Cart.js
@@ -5,7 +5,7 @@ const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: "2", price: 12.99 }].map((item) => (
-        <li>{}item.name</li>
+        <li>{item.name}</li>
       ))}
     </ul>
   );
@@ -16,7 +16,7 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div>
+      <div className={classes.actions}>
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
