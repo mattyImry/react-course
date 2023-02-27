933946bd335a257dca7af5288219c5ca80c9f926
Matteo Imeri <matteo.imeri@gmail.com>
2023-02-27 11:58:14 +0000

    refactor Input.js comp

--- useReducer2/src/components/Home/Home.js
+++ useReducer2/src/components/Home/Home.js
@@ -10,7 +10,7 @@ const Home = (props) => {
     return (
         <Card className={classes.home}>
             <h1>Welcome back!</h1>
-            <Button onClick={authCtx.onLogout}></Button>
+            <Button onClick={authCtx.onLogout}>Log Out</Button>
         </Card>
     );
 };
