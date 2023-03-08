21131715c919d5679ba7d006e6bbc24aac08256c
Matteo Imeri <matteo.imeri@gmail.com>
2023-03-08 11:10:35 +0000

    refactor jsx code App.js

--- http_req/src/App.js
+++ http_req/src/App.js
@@ -34,17 +34,26 @@ function App() {
         setIsLoading(false);
     }
 
+    let content = <p>No movies found</p>;
+
+    if (movies.length > 0) {
+        content = <MoviesList movies={movies} />;
+    }
+
+    if (error) {
+        content = <p>{error}</p>;
+    }
+
+    if (isLoading) {
+        content = <p>Loading...</p>;
+    }
+
     return (
         <React.Fragment>
             <section>
                 <button onClick={fetchMovieHandler}>Fetch Movies</button>
             </section>
-            <section>
-                {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-                {!isLoading && movies.length === 0 && !error && <p>Found no movies!</p>}
-                {isLoading && <p>Loading...</p>}
-                {!isLoading && error && <p>{error}</p>}
-            </section>
+            <section>{content}</section>
         </React.Fragment>
     );
 }
