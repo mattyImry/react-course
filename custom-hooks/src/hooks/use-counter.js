c1fda1efce4443cb9644a640e29cbfa3a6955c96
Matteo Imeri <matteo.imeri@gmail.com>
2023-03-23 11:16:18 +0000

    applied useCounter in ForwardCounter.js

--- custom-hooks/src/hooks/use-counter.js
+++ custom-hooks/src/hooks/use-counter.js
@@ -1,16 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const useCounter = () => {
-    const ForwardCounter = () => {
-        const [counter, setCounter] = useState(0);
-      
-        useEffect(() => {
-          const interval = setInterval(() => {
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
             setCounter((prevCounter) => prevCounter + 1);
-          }, 1000);
-      
-          return () => clearInterval(interval);
-        }, []);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return counter;
 };
 
 export default useCounter;
