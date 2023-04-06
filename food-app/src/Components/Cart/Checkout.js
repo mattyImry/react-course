cf9612ec36d35f7123e700b410881d2063026f91
Matteo Imeri <matteo.imeri@gmail.com>
2023-04-06 10:04:04 +0100

    add div to Checkout.js for styling

--- food-app/src/Components/Cart/Checkout.js
+++ food-app/src/Components/Cart/Checkout.js
@@ -4,8 +4,9 @@ const Checkout = (props) => {
     const confirmHandler = (event) => {
         event.preventDefault();
     };
+
     return (
-        <form onSubmit={confirmHandler}>
+        <form className={classes.form} onSubmit={confirmHandler}>
             <div className={classes.control}>
                 <label htmlFor="name">Your Name</label>
                 <input type="text" id="name" />
@@ -22,10 +23,12 @@ const Checkout = (props) => {
                 <label htmlFor="city">City</label>
                 <input type="text" id="city" />
             </div>
-            <button type="button" onClick={props.onCancel}>
-                Cancel
-            </button>
-            <button>Confirm</button>
+            <div className={classes.actions}>
+                <button type="button" onClick={props.onCancel}>
+                    Cancel
+                </button>
+                <button className={classes.submit}>Confirm</button>
+            </div>
         </form>
     );
 };
