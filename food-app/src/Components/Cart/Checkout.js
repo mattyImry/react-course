c882b2426675723534a47ac8958f4d7122a23182
Matteo Imeri <matteo.imeri@gmail.com>
2023-04-06 09:55:41 +0100

    set cancel from Modal

--- food-app/src/Components/Cart/Checkout.js
+++ food-app/src/Components/Cart/Checkout.js
@@ -3,24 +3,26 @@ import classes from "./Checkout.module.css";
 const Checkout = (props) => {
     return (
         <form>
-            <div classeName={classes.control}>
+            <div className={classes.control}>
                 <label htmlFor="name">Your Name</label>
                 <input type="text" id="name" />
             </div>
-            <div classeName={classes.control}>
+            <div className={classes.control}>
                 <label htmlFor="street">Street</label>
                 <input type="text" id="street" />
             </div>
-            <div classeName={classes.control}>
+            <div className={classes.control}>
                 <label htmlFor="postal">Postal Code</label>
                 <input type="text" id="postal" />
             </div>
-            <div classeName={classes.control}>
+            <div className={classes.control}>
                 <label htmlFor="city">City</label>
                 <input type="text" id="city" />
             </div>
+            <button type="button" onClick={props.onCancel}>
+                Cancel
+            </button>
             <button>Confirm</button>
-            <button>Cancel</button>
         </form>
     );
 };
