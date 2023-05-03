e0ecca3c23eb4daf6c656786212e4da4b64ee03f
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-03 11:34:27 +0100

    set product data

--- redux_adv/src/components/Shop/Products.js
+++ redux_adv/src/components/Shop/Products.js
@@ -1,19 +1,37 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+    {
+        id: "P1",
+        price: 8,
+        title: "book",
+        description: "The main book",
+    },
+    {
+        id: "P2",
+        price: 4,
+        title: "pen",
+        description: "The pen",
+    },
+];
 
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+    return (
+        <section className={classes.products}>
+            <h2>Buy your favorite products</h2>
+            <ul>
+                {DUMMY_PRODUCTS.map((product) => (
+                    <ProductItem
+                        key={product.id}
+                        title={product.title}
+                        price={product.price}
+                        description={product.description}
+                    />
+                ))}
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
