fff5ff2e64caf8d474a6c612d5de9fffd5588ab3
mattyImry <matteo.imeri@gmail.com>
2023-06-15 10:27:37 +0100

    fix bug in index.js store

--- redux_app_2/src/store/index.js
+++ redux_app_2/src/store/index.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from "./counter";
-import authReducer from "./counter";
+import authReducer from "./auth";
 
 const store = configureStore({
   reducer: { counter: counterReducer, auth: authReducer },
