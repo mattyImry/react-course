e655d895c411815f3d3eee56fae0cafe984a316d
mattyImry <matteo.imeri@gmail.com>
2023-06-30 11:28:21 +0100

    dispatch action for toggling cart

--- redux_adv2/src/store/index.js
+++ redux_adv2/src/store/index.js
@@ -3,7 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from "./ui-slice";
 
 const store = configureStore({
-  reducer: { ui: uiSlice.reducer() },
+  reducer: { ui: uiSlice.reducer },
 });
 
 export default store;
