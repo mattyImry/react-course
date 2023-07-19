7e04ecca8f945f88d55e9ec2c903b83c122d2501
Matteo Imeri <matteo.imeri@gmail.com>
2023-07-19 10:44:28 +0100

    start redux adv2

--- redux_adv2/src/App.js
+++ redux_adv2/src/App.js
@@ -1,14 +1,11 @@
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-
-import { useSelector } from "react-redux";
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
   return (
     <Layout>
-      {showCart && <Cart />}
+      <Cart />
       <Products />
     </Layout>
   );
--- redux_adv2/src/components/Cart/CartButton.js
+++ redux_adv2/src/components/Cart/CartButton.js
@@ -1,16 +1,8 @@
-import classes from "./CartButton.module.css";
-import { uiActions } from "../../store/ui-slice";
-import { useDispatch } from "react-redux";
+import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
-  const dispatch = useDispatch();
-
-  const toggleCartHandler = () => {
-    dispatch(uiActions.toggle());
-  };
-
   return (
-    <button className={classes.button} onClick={toggleCartHandler}>
+    <button className={classes.button}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
