f3f0ce36a2b8cb41546fe037a3ba7ec6cdee263e
Matteo Imeri <matteo.imeri@gmail.com>
2023-07-29 15:07:47 +0100

    add showNotification to ui-slice dispatch in http

--- redux_adv2/src/App.js
+++ redux_adv2/src/App.js
@@ -1,15 +1,25 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect } from "react";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
     const showCart = useSelector((state) => state.ui.cartIsVisible);
     const cart = useSelector((state) => state.cart);
+    const notification = useSelector((state) => state.ui.showNotification);
+    const dispatch = useDispatch();
     useEffect(() => {
         const sendCartData = async () => {
+            dispatch(
+                uiActions.showNotification({
+                    status: "pending",
+                    title: "Sending...",
+                    message: "Sending cart data",
+                })
+            );
             const response = await fetch(
                 "https://react-food-app-32373-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
                 { method: "PUT", body: JSON.stringify(cart) }
@@ -18,9 +28,25 @@ function App() {
             if (!response.ok) {
                 throw new Error("Sending cart data failed");
             }
-            const responseData = await response.json();
+            dispatch(
+                uiActions.showNotification({
+                    status: "success",
+                    title: "Success!",
+                    message: "Send cart data successfully",
+                })
+            );
+
+            sendCartData().catch((error) => {
+                dispatch(
+                    uiActions.showNotification({
+                        status: "error",
+                        title: "Error!",
+                        message: "Sending cart data failed!",
+                    })
+                );
+            });
         };
-    }, [cart]);
+    }, [cart, dispatch]);
     return (
         <Layout>
             {showCart && <Cart />}
