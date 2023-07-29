328f369e2f2843bd27d2eee1966a0243f42b49b5
Matteo Imeri <matteo.imeri@gmail.com>
2023-07-29 14:39:23 +0100

    http request using cart state in app.js

--- redux_adv2/src/App.js
+++ redux_adv2/src/App.js
@@ -3,9 +3,17 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
     const showCart = useSelector((state) => state.ui.cartIsVisible);
+    const cart = useSelector((state) => state.cart);
+    useEffect(() => {
+        fetch(
+            "https://react-food-app-32373-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+            { method: "PUT", body: JSON.stringify(cart) }
+        );
+    }, [cart]);
     return (
         <Layout>
             {showCart && <Cart />}
