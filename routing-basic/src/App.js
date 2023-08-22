2b14420907322fdde75fbaadcda4d0e941da0b5b
Matteo Imeri <matteo.imeri@gmail.com>
2023-08-22 11:33:12 +0100

    add routes

--- routing-basic/src/App.js
+++ routing-basic/src/App.js
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import Products from "./pages/Products";
+
+const routerHome = createBrowserRouter([
+    { path: "/", element: <HomePage /> },
+    { path: "products", element: <Products /> },
+]);
+
 function App() {
-  return <div></div>;
+    return <RouterProvider router={routerHome} />;
 }
 
 export default App;
