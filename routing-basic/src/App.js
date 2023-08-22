60fc8f5f81fbc195c356b4322f294b5d6b07b42a
Matteo Imeri <matteo.imeri@gmail.com>
2023-08-22 11:46:44 +0100

    alternative way before updating router

--- routing-basic/src/App.js
+++ routing-basic/src/App.js
@@ -1,6 +1,21 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//COMMENTED OUT CODE IS AALTERNATIVE WAY BEOFRE UPDATE ON ROUTER
+
+import {
+    createBrowserRouter,
+    //createRoutesFromElements,
+    //Route,
+    RouterProvider,
+} from "react-router-dom";
 import HomePage from "./pages/Home";
 import Products from "./pages/Products";
+
+// const routesDefinition = createRoutesFromElements(
+//     <Route>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/products" element={<Products />} />
+//     </Route>
+// );
+//const router = createBrowserRouter(routesDefinition);
 
 const routerHome = createBrowserRouter([
     { path: "/", element: <HomePage /> },
