e737b8a154217425633db1603c69d32b605c1395
Matteo Imeri <matteo.imeri@gmail.com>
2023-08-22 09:32:47 +0100

    refactor cart-slice for Thunk action creation

--- redux_adv2/src/App.js
+++ redux_adv2/src/App.js
@@ -15,51 +15,7 @@ function App() {
     const cart = useSelector((state) => state.cart);
     const notification = useSelector((state) => state.ui.notification);
 
-    useEffect(() => {
-        const sendCartData = async () => {
-            dispatch(
-                uiActions.showNotification({
-                    status: "pending",
-                    title: "Sending...",
-                    message: "Sending cart data!",
-                })
-            );
-            const response = await fetch(
-                "https://react-food-app-32373-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-                {
-                    method: "PUT",
-                    body: JSON.stringify(cart),
-                }
-            );
-
-            if (!response.ok) {
-                throw new Error("Sending cart data failed.");
-            }
-
-            dispatch(
-                uiActions.showNotification({
-                    status: "success",
-                    title: "Success!",
-                    message: "Sent cart data successfully!",
-                })
-            );
-        };
-
-        if (isInitial) {
-            isInitial = false;
-            return;
-        }
-
-        sendCartData().catch((error) => {
-            dispatch(
-                uiActions.showNotification({
-                    status: "error",
-                    title: "Error!",
-                    message: "Sending cart data failed!",
-                })
-            );
-        });
-    }, [cart, dispatch]);
+    useEffect(() => {}, [cart, dispatch]);
 
     return (
         <Fragment>
