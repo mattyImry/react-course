755f14f23497b4bf5b3b959cd7fa6a3ba637f5b8
Matteo Imeri <matteo.imeri@gmail.com>
2023-08-24 10:33:28 +0100

    add pages folder

--- route-prj/frontend/src/App.js
+++ route-prj/frontend/src/App.js
@@ -21,11 +21,11 @@
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import EventPage from "./components/EventsPage";
-import EventDetailPage from "./components/EventDetailPage";
-import NewEventPage from "./components/NewEventPage";
-import EditEventPage from "./components/EditEventPage";
+import HomePage from "./pages/HomePage";
+import EventPage from "./pages/EventsPage";
+import EventDetailPage from "./pages/EventDetailPage";
+import NewEventPage from "./pages/NewEventPage";
+import EditEventPage from "./pages/EditEventPage";
 
 const routerHome = createBrowserRouter([
     {
