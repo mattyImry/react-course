5706411e8a165f721300412a1b6687afe4542553
Matteo Imeri <matteo.imeri@gmail.com>
2023-08-24 11:25:34 +0100

    otput events and links

--- route-prj/frontend/src/pages/EventDetailPage.js
+++ route-prj/frontend/src/pages/EventDetailPage.js
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 function EventDetailPage() {
+    const params = useParams();
     return (
         <>
             <h1>Events Details</h1>
+            <p>Event Details: Id {params.eventId}</p>
         </>
     );
 }
--- route-prj/frontend/src/pages/EventsPage.js
+++ route-prj/frontend/src/pages/EventsPage.js
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
+const EVENTS = [
+    { id: "1", title: "event 1" },
+    { id: "2", title: "event 2" },
+    { id: "3", title: "event 3" },
+    { id: "4", title: "event 4" },
+];
+
 function EventPage() {
     return (
         <>
             <h1>My Event Page</h1>
+            <ul>
+                {EVENTS.map((event) => (
+                    <li key={event.id}>
+                        <Link to={event.id}>{event.title}</Link>
+                    </li>
+                ))}
+            </ul>
         </>
     );
 }
