7081210be90d152c2140c2219f8bb773865d955e
Matteo Imeri <matteo.imeri@gmail.com>
2023-08-24 10:14:11 +0100

    add HomePage.js

--- route-prj/frontend/src/App.js
+++ route-prj/frontend/src/App.js
@@ -21,7 +21,7 @@
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 function App() {
-  return <div></div>;
+    return <div></div>;
 }
 
 export default App;
