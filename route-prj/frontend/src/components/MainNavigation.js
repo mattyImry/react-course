b3ad8dfd64886ad79babd3ac6c0f026ed7cb0b4d
Matteo Imeri <matteo.imeri@gmail.com>
2023-08-24 10:44:13 +0100

    add NavLink to MainNavigation.js

--- route-prj/frontend/src/components/MainNavigation.js
+++ route-prj/frontend/src/components/MainNavigation.js
@@ -1,20 +1,24 @@
-import classes from './MainNavigation.module.css';
+import { NavLink } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
-  return (
-    <header className={classes.header}>
-      <nav>
-        <ul className={classes.list}>
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>Events</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={classes.header}>
+            <nav>
+                <ul className={classes.list}>
+                    <li>
+                        <NavLink to="" end>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="events">Events</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default MainNavigation;
