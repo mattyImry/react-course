794b2d44fce10de12e5609663657eb2a46f80e6d
Matteo Imeri <matteo.imeri@gmail.com>
2023-08-30 09:00:12 +0100

    add Newsletter.js

--- route-prj/frontend/src/App.js
+++ route-prj/frontend/src/App.js
@@ -1,17 +1,40 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// Challenge / Exercise
 
-import EditEventPage from "./pages/EditEvent";
-import ErrorPage from "./pages/Error";
+///// 1. Add five new (dummy) page components (content can be simple <h1> elements)
+////  - HomePage
+////  - EventsPage
+////  - EventDetailPage
+////  - NewEventPage
+////  - EditEventPage
+////// 2. Add routing & route definitions for these five pages
+//////    - / => HomePage
+//////    - /events => EventsPage
+//////    - /events/<some-id> => EventDetailPage
+//////    - /events/new => NewEventPage
+//////    - /events/<some-id>/edit => EditEventPage
+////// 3. Add a root layout that adds the <MainNavigation> component above all page components
+///// 4. Add properly working links to the MainNavigation
+//// 5. Ensure that the links in MainNavigation receive an "active" class when active
+//// 6. Output a list of dummy events to the EventsPage
+////    Every list item should include a link to the respective EventDetailPage
+// 7. Output the ID of the selected event on the EventDetailPage
+// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import EventsPage, { loader as eventsLoader } from "./pages/EventsPage";
 import EventDetailPage, {
     loader as eventDetailLoader,
     action as deleteEventAction,
-} from "./pages/EventDetail";
-import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventsRootLayout from "./pages/EventsRoot";
-import HomePage from "./pages/Home";
-import NewEventPage from "./pages/NewEvent";
-import RootLayout from "./pages/Root";
+} from "./pages/EventDetailPage";
+import NewEventPage from "./pages/NewEventPage";
+import EditEventPage from "./pages/EditEventPage";
+import RootLayout from "./pages/RootLayout";
+import EventsRoot from "./pages/EventsRoot";
+import ErrorPage from "./pages/ErrorPage";
+
 import { action as manipulateEventAction } from "./components/EventForm";
+
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 
 const router = createBrowserRouter([
@@ -23,7 +46,7 @@ const router = createBrowserRouter([
             { index: true, element: <HomePage /> },
             {
                 path: "events",
-                element: <EventsRootLayout />,
+                element: <RootLayout />,
                 children: [
                     {
                         index: true,
