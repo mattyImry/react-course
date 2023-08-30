c535ad18c56f736460230660182ea9e20efdc43e
Matteo Imeri <matteo.imeri@gmail.com>
2023-08-30 10:23:26 +0100

    add defer to events page

--- route-prj/frontend/src/pages/EventsPage.js
+++ route-prj/frontend/src/pages/EventsPage.js
@@ -1,19 +1,23 @@
-import { useLoaderData, json } from "react-router-dom";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
-    const data = useLoaderData();
-    // if (data.isError) {
-    //     return <p>{data.message}</p>;
-    // }
-    const events = data.events;
-    return <EventsList events={events} />;
+    const { events } = useLoaderData();
+
+    return (
+        <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+            <Await resolve={events}>
+                {(loadedEvents) => <EventsList events={loadedEvents} />}
+            </Await>
+        </Suspense>
+    );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
     const response = await fetch("http://localhost:8080/events");
 
     if (!response.ok) {
@@ -21,13 +25,20 @@ export async function loader() {
         // throw new Response(JSON.stringify({ message: "Could not fecth event" }), {
         //     status: 500,
         // });
-        return json(
+        throw json(
             { message: "Could not fecth event" },
             {
                 status: 500,
             }
         );
     } else {
-        return response;
+        const resData = await response.json();
+        return resData.events;
     }
+}
+
+export function loader() {
+    defer({
+        events: loadEvents(),
+    });
 }
