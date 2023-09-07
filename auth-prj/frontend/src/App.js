4c45aa2960355de99d4193b6350f7c3e687b10d0
matty <matteo.imeri@gmail.com>
2023-09-07 10:05:35 +0100

    add auth path app.js

--- auth-prj/frontend/src/App.js
+++ auth-prj/frontend/src/App.js
@@ -11,6 +11,7 @@ import EventsRootLayout from './pages/EventsRoot';
 import HomePage from './pages/Home';
 import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
+import Authentication from'./pages/Authentication'
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
@@ -54,6 +55,8 @@ const router = createBrowserRouter([
           },
         ],
       },
+      {path:'auth',
+      element:<Authentication/>},
       {
         path: 'newsletter',
         element: <NewsletterPage />,
