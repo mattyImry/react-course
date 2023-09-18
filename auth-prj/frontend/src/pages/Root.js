e7ad4c55b7e5b47ebe3cccf99f9669f75ba75aaa
Matteo Imeri <matteo.imeri@gmail.com>
2023-09-18 14:44:59 +0100

    auto logout

--- auth-prj/frontend/src/pages/Root.js
+++ auth-prj/frontend/src/pages/Root.js
@@ -1,19 +1,31 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+    // const navigation = useNavigation();
+    const token = useLoaderData();
+    const submit = useSubmit();
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
 
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+        setTimeout(() => {
+            submit(null, { action: "/logout", method: "post" });
+        }, 1 * 60 * 60 * 1000);
+    }, [token, submit]);
+
+    return (
+        <>
+            <MainNavigation />
+            <main>
+                {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+                <Outlet />
+            </main>
+        </>
+    );
 }
 
 export default RootLayout;
