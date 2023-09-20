422b68d7b510908f4a77660c3787ee678c0644a6
Matteo Imeri <matteo.imeri@gmail.com>
2023-09-20 15:33:55 +0100

    set useQuery  FindEventSection.js

--- tanstack/src/components/Events/FindEventSection.jsx
+++ tanstack/src/components/Events/FindEventSection.jsx
@@ -1,34 +1,37 @@
-import { useQuery } from '@tanstack/react-query';
-import { useRef } from 'react';
+import { useQuery } from "@tanstack/react-query";
+import { useRef, useState } from "react";
+import { fetchEvents } from "../../util/http.js";
 
 export default function FindEventSection() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const searchElement = useRef();
 
-  const j = useQuery({
-        queryKey: ["events"],
-        queryFn:
-  })
+    const { data, isPending, isError, error } = useQuery({
+        queryKey: ["events", { search: searchTerm }],
+        queryFn: () => fetchEvents(searchTerm),
+    });
 
-  
-  const searchElement = useRef();
+    function handleSubmit(event) {
+        event.preventDefault();
+        setSearchTerm(searchElement.current.value);
+    }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+    let 
 
-  return (
-    <section className="content-section" id="all-events-section">
-      <header>
-        <h2>Find your next event!</h2>
-        <form onSubmit={handleSubmit} id="search-form">
-          <input
-            type="search"
-            placeholder="Search events"
-            ref={searchElement}
-          />
-          <button>Search</button>
-        </form>
-      </header>
-      <p>Please enter a search term and to find events.</p>
-    </section>
-  );
+    return (
+        <section className="content-section" id="all-events-section">
+            <header>
+                <h2>Find your next event!</h2>
+                <form onSubmit={handleSubmit} id="search-form">
+                    <input
+                        type="search"
+                        placeholder="Search events"
+                        ref={searchElement}
+                    />
+                    <button>Search</button>
+                </form>
+            </header>
+            <p>Please enter a search term and to find events.</p>
+        </section>
+    );
 }
