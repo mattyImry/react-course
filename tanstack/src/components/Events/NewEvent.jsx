555e841be6e9351362e1b25dae97b75bcf850a20
Matteo Imeri <matteo.imeri@gmail.com>
2023-09-20 16:39:56 +0100

    display user messages NewEvent.jsx

--- tanstack/src/components/Events/NewEvent.jsx
+++ tanstack/src/components/Events/NewEvent.jsx
@@ -4,6 +4,7 @@ import { useMutation } from "@tanstack/react-query";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 import { createNewEvent } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
     const navigate = useNavigate();
@@ -19,15 +20,24 @@ export default function NewEvent() {
     return (
         <Modal onClose={() => navigate("../")}>
             <EventForm onSubmit={handleSubmit}>
-                <>
-                    <Link to="../" className="button-text">
-                        Cancel
-                    </Link>
-                    <button type="submit" className="button">
-                        Create
-                    </button>
-                </>
+                {isPending && "Submitting...."}
+                {!isPending && (
+                    <>
+                        <Link to="../" className="button-text">
+                            Cancel
+                        </Link>
+                        <button type="submit" className="button">
+                            Create
+                        </button>
+                    </>
+                )}
             </EventForm>
+            {isError && (
+                <ErrorBlock
+                    title="Failed to create event"
+                    message={error.info?.message || "Failed to create event."}
+                />
+            )}
         </Modal>
     );
 }
